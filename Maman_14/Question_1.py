16523d54c567f25ecd0f86329e46073b24f13434
kdjmilov <kdjmilov@kdjmilov-MOBL4.ger.corp.intel.com>
2018-05-14 15:22:05 +0300

    progress

--- Maman_14/Question_1.py
+++ Maman_14/Question_1.py
@@ -4,40 +4,44 @@ Created on May 7, 2018
 @author: Kosta
 '''
 import sys
-#hi Kosta
+
 def startTheProg():
     arrayMin = list()
     arrayMax = list()
-    numbers = raw_input("Please enter how many numbers do you want\n")
-    print "Please enter the numbers"
+    print 'Welcome to Maman 14'
+    numbers = raw_input("Please enter the amount of numbers for the data base\n")
     for i in range(int(numbers)):
         num = raw_input("num "+str(i+1)+" :")
         arrayMin.append(int(num))
         arrayMax.append(int(num))
-    print 'The array is: ',arrayMin
-    print 'The array is: ',arrayMax
+    #print 'The Max array is: ',arrayMin
+    #print 'The Min array is: ',arrayMax
     return arrayMin, arrayMax
 
+#Max heap function. Receives the array minimum and return the maximum sorted heap.
+#Time complexity is o(n)
 def buildMaxHeap(arrMax):
     array_len = len(arrMax)
     for i in range(array_len,-1,-1):
         heapifyMax(arrMax,array_len,i)
     print "Max heap is: ",
-    for i in range(array_len):
-        print("%d " %arrMax[i]),
+    printArray(arrMax)
     print '\n'
     return arrMax
 
+#Min heap function. Receives the array minimum and return the minimum sorted heap. 
+#Time complexity is o(n)
 def buildMinHeap(arrMin):
     array_len = len(arrMin)
     for i in range(array_len,-1,-1):
         heapifyMin(arrMin,array_len,i)
     print "Min heap is: ",
-    for i in range(array_len):
-        print("%d " %arrMin[i]),
+    printArray(arrMin)
     print '\n'
     return arrMin
 
+#Insert function. Gets the number to insert and the 2 arrays
+#This function will split the insert action for min heap and for max heap
 def insert(number,arrMax,arrMin):
     arrMax.append(int(number))
     arrMin.append(int(number))
@@ -48,7 +52,9 @@ def insert(number,arrMax,arrMin):
     if arrMin[lenOfArrayMin-1]<arrMin[((lenOfArrayMax-1)/2)]:
         arrMin = insertMin(arrMin,lenOfArrayMin-1)
     return arrMax, arrMin
-        
+
+#Insert Max function. Gets the relevant array and the index to start. The function returns the new array.
+#Time complexity is o(lgn)        
 def insertMax(arr,index):
     if index == 0:
         return
@@ -58,6 +64,8 @@ def insertMax(arr,index):
             insertMax(arr,((index-1)/2))
     return arr
 
+#Insert Min function. Gets the relevant array and the index to start. The function returns the new array.
+#Time complexity is o(lgn)  
 def insertMin(arr,index):
     if index == 0:
         return
@@ -67,8 +75,13 @@ def insertMin(arr,index):
             insertMin(arr,((index-1)/2))
     return arr
 
+#Finds max number. Receives relevant array and return first index.
+#Time complexity is o(1)   
 def find_max(arr):
     return arr[0]
+
+#Finds min number. Receives relevant array and return first index.
+#Time complexity is o(1)  
 def find_min(arr):
     return arr[0]
     
@@ -117,12 +130,29 @@ def deletMin(arr):
         del arr[len(arr)-1]
         heapifyMin(arr,len(arr)-1,0)
         return arr,min_num
-        
+    
+def printArray(arr):
+    for i in range(len(arr)):
+        print("%d " %arr[i]),
+    
+
+def reOrgTheArray(arr):
+    del arr[len(arr)-1]
+    return arr
+
 if __name__ == "__main__":
     arrayMin, arrayMax = startTheProg()
-    while 1: 
-        user_option = raw_input("\n-------------------------\nTo build a heap press 1\nTo insert a num press 2\nTo find Max press 3\n"+
-        "To find Min press 4\nTo delet Max press 5\nTo delet Min press 6\n")
+    while 1:
+        print '\n'
+        print "-----Menu-----" 
+        print "Press 1 to build a Heap"
+        print "Press 2 to insert a number"
+        print "Press 3 to find Max"
+        print "Press 4 to find Min"
+        print "Press 5 to delet Max"
+        print "Press 6 to delete Min"
+        print "Press 999 to end the program"
+        user_option = raw_input()
         if user_option == str(1):
             arrayMax = buildMaxHeap(arrayMax)
             arrayMin = buildMinHeap(arrayMin)
@@ -130,25 +160,33 @@ if __name__ == "__main__":
             user_option = raw_input("please insert a number\n\n")
             arrayMaxAfterInsert,arrayMinAfterInsert = insert(user_option,arrayMax, arrayMin)
             print "New Max array after insert action is: "
-            for i in range(len(arrayMaxAfterInsert)):
-                print("%d " %arrayMaxAfterInsert[i]),
-            arrayMax = arrayMaxAfterInsert
             print "\n"
+            printArray(arrayMaxAfterInsert)
+            arrayMax = arrayMaxAfterInsert
             print "New Min array after insert action is: "
-            for i in range(len(arrayMinAfterInsert)):
-                print("%d " %arrayMinAfterInsert[i]),
+            print "\n"
+            printArray(arrayMinAfterInsert)
             arrayMin = arrayMinAfterInsert
+            
         if user_option == str(3):
-            print find_max(arrayMax)
+            max_num = find_max(arrayMax)
+            print 'Maximum number is: ' + str(max_num)
         if user_option == str(4):
-            print find_min(arrayMin)
+            min_num = find_min(arrayMin)
+            print 'Minimum number is: ' + str(min_num)
         if user_option == str(5):
             arrayMax, deletedMaxNum = deletMax(arrayMax)
-            print arrayMax
             print 'The deleted maximum is: '+ str(deletedMaxNum)
+            arrayMin = reOrgTheArray(arrayMin)
+            print 'Max heap after delet Max' + str(arrayMax)
+            print 'Min heap after delet Max' + str(arrayMin)
         if user_option == str(6):
             arrayMin, deletedMinNum = deletMin(arrayMin)
-            print arrayMin
             print 'The deleted minimum is: '+ str(deletedMinNum)
-            
+            arrayMax = reOrgTheArray(arrayMax)
+            print 'Max heap after delet Max' + str(arrayMax)
+            print 'Min heap after delet Max' + str(arrayMin)
+        if user_option == str(999):
+            print '999 was pressed, ending the program'
+            sys.exit()
         
